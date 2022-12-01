8dbcf7d880ff115d2131febd5408b3de184a0f5e
Steve Sosik <ssosik@gmail.com>
2022-12-01 18:41:37 -0500

    day1 part2 complete

--- src/day1.rs
+++ src/day1.rs
@@ -19,73 +19,35 @@ pub fn part1(input: &[i32]) -> i32 {
     input.iter().fold(std::i32::MIN, |a,b| a.max(*b))
 }
 
-//#[aoc(day1, part2)]
-//pub fn part2(input: Vec<i32>) -> usize {
-//    let mut sum: u32 = 0;
-//
-//    for (i, c) in input.as_bytes().iter().enumerate() {
-//        match c {
-//            b'(' => sum += 1,
-//            b')' => if let Some(s) = sum.checked_sub(1) {
-//                sum = s;
-//            } else {
-//                return i + 1;
-//            },
-//            _ => unreachable!(),
-//        }
-//    }
-//
-//    unreachable!()
-//}
+#[aoc(day1, part2)]
+pub fn part2(input: &[i32]) -> i32 {
+    let mut first = &std::i32::MIN;
+    let mut second = &std::i32::MIN;
+    let mut third = &std::i32::MIN;
+
+    for x in input {
+        if x > first {
+            third = second;
+            second = first;
+            first = x;
+        } else if x > second {
+            third = second;
+            second = x;
+        } else if x > third {
+            third = x;
+        }
+    }
+    first + second + third
+}
 
 #[cfg(test)]
 mod tests {
     use super::{part1, part2};
 
-    // (()) and ()() both result in floor 0.
-    #[test]
-    fn sample1() {
-        assert_eq!(part1("(())"), 0);
-        assert_eq!(part1("()()"), 0);
-    }
-
-    // ((( and (()(()( both result in floor 3.
-    #[test]
-    fn sample2() {
-        assert_eq!(part1("((("), 3);
-        assert_eq!(part1("(()(()("), 3);
-    }
-
-    // ))((((( also results in floor 3.
-    #[test]
-    fn sample3() {
-        assert_eq!(part1("))((((("), 3);
-    }
-
-    // ()) and ))( both result in floor -1 (the first basement level).
-    #[test]
-    fn sample4() {
-        assert_eq!(part1("())"), -1);
-        assert_eq!(part1("))("), -1);
-    }
-
-    // ))) and )())()) both result in floor -3.
-    #[test]
-    fn sample5() {
-        assert_eq!(part1(")))"), -3);
-        assert_eq!(part1(")())())"), -3);
-    }
-
-    // ) causes him to enter the basement at character position 1.
-    #[test]
-    fn sample6() {
-        assert_eq!(part2(")"), 1);
-    }
-
-    // ()()) causes him to enter the basement at character position 5.
-    #[test]
-    fn sample7() {
-        assert_eq!(part2("()())"), 5);
-    }
+    //// (()) and ()() both result in floor 0.
+    //#[test]
+    //fn sample1() {
+    //    assert_eq!(part1("(())"), 0);
+    //    assert_eq!(part1("()()"), 0);
+    //}
 }
-
