9e9f9f8fb4e0135a1af34ee57595af37a32ee9af
Steve Sosik <ssosik@gmail.com>
2022-12-02 15:48:28 -0500

    day2 part1 solved

--- src/day2.rs
+++ src/day2.rs
@@ -1,43 +1,57 @@
-#[aoc_generator(day2)]
-pub fn input_generator(input: &str) -> Vec<i32> {
-    let mut result = Vec::new();
-    let mut curr = 0;
-    for line in input.lines() {
-        let line = line.trim();
-        if line.is_empty() {
-            result.push(curr);
-            curr = 0;
-            continue;
-        }
-        curr += line.parse::<i32>().unwrap();
-    }
-    result
-}
+// A - Rock, 1
+// B - Paper, 2
+// C - Scissor, 3
+// X - Rock, 1
+// Y - Paper, 2
+// Z - Scissor, 3
+const ROCK: i32 = 1;
+const PAPER: i32 = 2;
+const SCISSORS: i32 = 3;
+const LOSS: i32 = 0;
+const TIE: i32 = 3;
+const WIN: i32 = 6;
 
 #[aoc(day2, part1)]
-pub fn part1(input: &[i32]) -> i32 {
-    input.iter().fold(std::i32::MIN, |a, b| a.max(*b))
+pub fn part1(input: &str) -> i32 {
+    input.lines().fold(0, |acc, line| {
+        acc + match line.split(' ').collect::<Vec<&str>>()[..] {
+            [a, b] => match (a, b) {
+                ("A", "X") => TIE + ROCK,
+                ("A", "Y") => WIN + PAPER,
+                ("A", "Z") => LOSS + SCISSORS,
+                ("B", "X") => LOSS + ROCK,
+                ("B", "Y") => TIE + PAPER,
+                ("B", "Z") => WIN + SCISSORS,
+                ("C", "X") => WIN + ROCK,
+                ("C", "Y") => LOSS + PAPER,
+                ("C", "Z") => TIE + SCISSORS,
+                _ => panic!("Invalid play"),
+            },
+            _ => panic!("Invalid line"),
+        }
+    })
 }
 
 #[aoc(day2, part2)]
-pub fn part2(input: &[i32]) -> i32 {
-    let mut first = &std::i32::MIN;
-    let mut second = &std::i32::MIN;
-    let mut third = &std::i32::MIN;
+pub fn part2(input: &str) -> i32 {
+    //let mut first = &std::i32::MIN;
+    //let mut second = &std::i32::MIN;
+    //let mut third = &std::i32::MIN;
 
-    for x in input {
-        if x > first {
-            third = second;
-            second = first;
-            first = x;
-        } else if x > second {
-            third = second;
-            second = x;
-        } else if x > third {
-            third = x;
-        }
-    }
-    first + second + third
+    //for x in input {
+    //    if x > first {
+    //        third = second;
+    //        second = first;
+    //        first = x;
+    //    } else if x > second {
+    //        third = second;
+    //        second = x;
+    //    } else if x > third {
+    //        third = x;
+    //    }
+    //}
+    //first + second + third
+    0
 }
 
 #[cfg(test)]
