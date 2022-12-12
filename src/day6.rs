867d5a079b08c88aeb0ee2cd7699076c9e6e889f
Steve Sosik <ssosik@gmail.com>
2022-12-11 21:05:58 -0500

    Day6 part2 done

--- src/day6.rs
+++ src/day6.rs
@@ -1,5 +1,9 @@
-use itertools::FoldWhile::{Continue, Done};
-use itertools::Itertools;
+use itertools::{
+    any,
+    FoldWhile::{Continue, Done},
+    Itertools,
+};
+use std::collections::HashSet;
 
 #[derive(Default)]
 pub struct Quad(Option<char>, Option<char>, Option<char>, Option<char>);
@@ -8,7 +12,7 @@ impl Quad {
     fn new() -> Self {
         Quad(None, None, None, None)
     }
-    fn start_of_packet(&self) -> bool {
+    fn is_all_unique(&self) -> bool {
         match self {
             Quad(None, _, _, _)
             | Quad(_, None, _, _)
@@ -26,15 +30,42 @@ impl Quad {
     }
 }
 
+pub struct StartMessageMarker([Option<char>; 14]);
+
+impl StartMessageMarker {
+    fn new() -> Self {
+        Self([None; 14])
+    }
+    fn is_all_unique(&self) -> bool {
+        if any(self.0, |item| item.is_none()) {
+            return false;
+        }
+        match self.0.iter().fold_while(HashSet::new(), |mut hs, item| {
+            if !hs.insert(item) {
+                Done(hs)
+            } else {
+                Continue(hs)
+            }
+        }) {
+            Continue(_) => true, // Ran off the end inserting, ALL UNIQUE
+            _ => false,          // One of the inserts was a dupe, NOT ALL UNIQUE
+        }
+    }
+    fn add(&mut self, c: char) {
+        for i in 0..13 {
+            self.0[i] = self.0[i + 1];
+        }
+        self.0[13] = Some(c);
+    }
+}
+
 #[aoc(day6, part1)]
 pub fn part1(input: &str) -> usize {
-    //input.chars().for_each(|x| println!("{x}"));
     let (i, _) = input
         .chars()
         .fold_while((1, Quad::new()), |(i, mut quad), next| {
-            println!("{i}: {next}");
             quad.add(next);
-            if quad.start_of_packet() {
+            if quad.is_all_unique() {
                 Done((i, quad))
             } else {
                 Continue((i + 1, quad))
@@ -44,9 +75,20 @@ pub fn part1(input: &str) -> usize {
     i
 }
 
-//#[aoc(day6, part2)]
+#[aoc(day6, part2)]
 pub fn part2(input: &str) -> usize {
-    0
+    let (i, _) = input
+        .chars()
+        .fold_while((1, StartMessageMarker::new()), |(i, mut window), next| {
+            window.add(next);
+            if window.is_all_unique() {
+                Done((i, window))
+            } else {
+                Continue((i + 1, window))
+            }
+        })
+        .into_inner();
+    i
 }
 
 #[cfg(test)]
